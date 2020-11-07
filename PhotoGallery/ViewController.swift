81b911362b6c8d3cf29af2ab6fb5b64cb9b8ad42
Eduard Galchenko <eddiegalchenko@gmail.com>
2020-11-07 16:27:27 +0200

    Initial Commit

---
+++ PhotoGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Eduard Galchenko on 07.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
