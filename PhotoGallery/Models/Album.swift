fc4b6297e7f483611642f63d9e14acbd6c9ddbaf
Eduard Galchenko <eddiegalchenko@gmail.com>
2020-11-07 17:58:44 +0200

    models added

---
+++ PhotoGallery/Models/Album.swift
@@ -0,0 +1,14 @@
+//
+//  Album.swift
+//  PhotoGallery
+//
+//  Created by Eduard Galchenko on 07.11.2020.
+//
+
+import Foundation
+
+struct Album {
+    let userId: Int
+    let id: Int
+    let title: String
+}
---
+++ PhotoGallery/Models/Photo.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  PhotoGallery
+//
+//  Created by Eduard Galchenko on 07.11.2020.
+//
+
+import Foundation
+
+struct Photo {
+    let albumId: Int
+    let id: Int
+    let title: String
+    let url: String
+    let thumbnailUrl: String
+}
